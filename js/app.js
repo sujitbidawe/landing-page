a033e8c4148f41bda73580e137a2ebb590f6929c
sujitbidawe <bidawesujit@gmail.com>
2021-11-30 23:46:24 +0530

    ADDED: functionality and styling for landing page

--- js/app.js
+++ js/app.js
@@ -1,82 +1,73 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// Global variables
+sections = [];
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-sections = [{name: "section1", isVisible: false}, {name: "section2", isVisible: false}, {name: "section3", isVisible: false}, {name: "section4", isVisible: false}];
-
+// Function to check element visibility
 function checkElementVisibility() {
-    const visibleSection = sections.find((section) => {
-        const ele = document.getElementById(section.name);
-        const rect = ele.getBoundingClientRect();
-        if( rect.top >= 0 && rect.left >= 0 ){
+    const visibleSection = sections.find((section, index) => {
+        const ele = document.getElementById(section.id);
+        const rect = ele && ele.getBoundingClientRect();
+        if( rect && rect.top >= 0 && rect.left >= 0 ){
+            section.classList.add("active__section");
             return section;
+        } else if (sections.length == index + 1) {
+            section.classList.add("active__section");
+            return section;
+        } else {
+            section.classList.remove("active__section");
         }
     });
-    visibleSection && sections.forEach(section => {
-        if (section.name === visibleSection.name) {
-            section.isVisible = true;
-        } else {
-            section.isVisible = false;
-        }
-    })
+    removeActiveClassFromNav();
+    targetActiveNavId = visibleSection && visibleSection.id + "-link";
+    let targetActiveNavEle = document.getElementById(targetActiveNavId);
+    targetActiveNavEle && targetActiveNavEle.classList.add("active")
 }
 
+// Function to remove active class from nav-link
+function removeActiveClassFromNav() {
+    const currentActiveEle = document.querySelector(`.active`);
+    currentActiveEle && currentActiveEle.classList.remove(`active`);
+}
+
+// Function to scroll to section of the page
+function scrollToSection(event) {
+    const scrollToId = event.target.getAttribute('scroll-to');
+    const scrollToNavId = scrollToId + "-link";
+    let scrollToNavEle = document.getElementById(scrollToNavId);
+    scrollToNavEle && scrollToNavEle.classList.add("active")
+    const scrollToEle = document.getElementById(scrollToId);
+    scrollToEle.scrollIntoView({ behavior: "smooth" });
+}
+
+// Function to generate nav elements based on available sections
+function generateNavElements(sectionNames) {
+    let docFragment = document.createDocumentFragment();
+    sectionNames && sectionNames.forEach((sectionName, index) => {
+        let listEle = document.createElement('li');
+        listEle.id = `${sectionName+'-link'}`;
+        listEle.innerText = `${sectionName}`;
+        listEle.className = (index === 0) ? 'sectionLink active' : 'sectionLink';
+        listEle.setAttribute('scroll-to', sectionName);
+        listEle.addEventListener('click', scrollToSection);
+        docFragment.appendChild(listEle);
+    });
+    let navbar = document.getElementById("navbar__list");
+    navbar.appendChild(docFragment);
+}
+
+// Function to identify sections of the page
+function identifySections() {
+    sections = [...document.getElementsByTagName('section')];
+    const sectionNames = sections.map(section => {
+        return section.id;
+    })
+    generateNavElements(sectionNames);
+}
+
+function scrollToTop(){
+    const pageHeader =  document.getElementById("heading");
+    pageHeader && pageHeader.scrollIntoView({ behavior: "smooth" });
+}
+
+// execution begins here
+identifySections();
 window.addEventListener("scroll", checkElementVisibility);
